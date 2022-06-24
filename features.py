4d3c51338f5c5612dceebbcc92b68d29ce65efd9
blessontomjoseph <blessontomjoseph@gmail.com>
2022-06-24 19:50:30 +0530

    features update

---
+++ features.py
@@ -0,0 +1,80 @@
+def team_val(team_data):
+    """finds the vector representing a team
+
+    Args:
+        team_data (dataframe): data containing only a teams info
+
+    Returns:
+        vector : team vector
+    """
+    weight=team_data.weight_val
+    ini_features = team_data.drop('weight_val').select_dtypes('number')
+    weighted_matrix = ini_features.apply(lambda x:x*weight)
+    vector = weighted_matrix.sum(axis=0)
+    normalized_vector = vector/(np.linalg.norm(vector))
+    return normalized_vector.values
+
+def pos_val(team_data,unique_position):
+    """vector reprsenting  a position in a team
+
+    Args:
+        team_data (dataframe): data rrepresenting a team
+        unique_position (series): all unique positions in a team
+
+    Returns:
+        vector: vector representing a position in a team
+    """
+    pos_data=team_data.loc[team_data['registered_position']==unique_position]
+    weight=pos_data.weight_val
+    ini_feat=pos_data.drop('weight_val').select_dtypes('number')
+    weighted_matrix= ini_feat.apply(lambda x:x*weight)
+    vector=weighted_matrix.sum(axis=0)
+    normalized_vector=vector/np.linalg.norm(vector)
+    return normalized_vector.values
+
+def player_val(team_data,player):
+    """player vector
+
+    Args:
+        team_data (dataframe): teamdata
+        player (int): single player index from data
+
+    Returns:
+        vctor: player vector
+    """
+    player_data=team_data.iloc[player]
+    vec=player_data.drop('weight_vals').select_dtypes('number')
+    n_vec=vec/np.linalg.norm(vec)
+    return n_vec.values
+
+
+
+
+def feature_eng(data,new_data):
+    """fills up the ralatonal values between a player and all the positions"""
+
+    for team in teams:
+        team_dict={}
+        positional_dict={}
+        player_dict={}
+        
+        team_data = data.loc[data['team_name']==team] #data for a unique team 
+        team_dict[team]= team_val(team_data)
+    
+        unique_positions=team_data.registered_position.unique()
+        for position in unique_positions:
+            positional_dict[position]=pos_val(team_data,position)
+        
+        for player in range(team_data.shape[0]):
+            name=team_data['name'].iloc[player]
+            player_dict[name]=player_val(team_data,player)
+            
+        for player in player_dict:
+            new_data['Team'][new_data['name']==player] = np.linalg.norm(team_dict.get(team)-player_dict.get(player))
+
+            for position in positional_dict:
+                new_data[position][new_data['name']==player] = np.linalg.norm(positional_dict.get(position)-player_dict.get(player))
+    
+    return new_data
+
+data_eng = feature_eng(regular,new_data)
--- preprocessing.py
+++ preprocessing.py
@@ -7,16 +7,8 @@ warnings.simplefilter('ignore')
 
 data_path = 'pes2021-all-players.csv'
 df= pd.read_csv(data_path)
-
-
-# com = df.columns[df.columns.str.contains('com')]  #1 or 0 only smal num of players has com abilities
-# skill = df.columns[df.columns.str.contains('skill')] # 1 or 0 only few of em has anny of the skills
-# positions=['LWF','SS','CF','RWF','LMF','DMF','CMF','AMF','RMF','LB','CB','RB']
-#unique positions,each players has a rating in every position from 0-2
-# all_rating=df.columns[df.columns.str.contains('rating')]
-
-
 regular=df.copy()
+
 regular.drop('rating_stars',1,inplace=True)
 dic={'---':0}
 regular['playing_style'].replace(dic,inplace=True)
@@ -24,14 +16,8 @@ regular['playing_style'].replace(dic,inplace=True)
 na=regular.isna().sum()
 nan_cols=na[na>0].index
 # these rows has nan values
-
-
 im=SimpleImputer(strategy='median')
 regular[nan_cols] =im.fit_transform(regular[nan_cols])
-
-
-# python preprocessing.py
-
 
 #foot
 foot_dict={'Left foot':0,'Right foot':1}
@@ -60,24 +46,17 @@ sc=StandardScaler()
 regular[numerical]=sc.fit_transform(regular[numerical])
 
 
-
 color=regular.ball_color.unique()
 a=[i for i in range(5,0,-1)]
 mapper={color[i]:a[i] for i in range(5)}
+
 
 regular['weight_val']=regular['ball_color']
 regular['weight_val']=regular['ball_color'].map(mapper)
 categorical= regular.select_dtypes('object').columns
 new_data=regular[categorical]
 
+
 extra=pd.DataFrame(np.zeros([new_data.shape[0],new_data.registered_position.unique().shape[0]+1]),columns=['RWF', 'LWF', 'CF', 'AMF', 'GK', 'CB', 'DMF', 'CMF', 'SS', 'LMF','RB', 'LB', 'RMF','Team'])
 new_data=pd.concat([new_data,extra],axis=1)
 print(new_data.head(5))
-
-
-
-# 
-# Host aws_ec2_first
-#     HostName ec2-54-245-143-111.us-west-2.compute.amazonaws.com
-#     IdentityFile C:\Users\USER\.ssh\aws_first_ec2.pem
-#     User ec2-user
