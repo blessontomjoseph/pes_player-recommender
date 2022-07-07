19d9a7a75981984797d53a0f3a8dbc9376b2869a
home2 <home2@iMac.local>
2022-07-08 00:13:53 +0530

    upd

---
+++ result.py
@@ -0,0 +1,11 @@
+from email import header
+from operator import index
+import preprocessing
+import features
+import pandas as pd
+
+
+data_path = 'pes2021-all-players.csv'
+regular,new_data=preprocessing.pre(data_path) #something like this
+data_eng = features.new_features(regular,new_data)
+data_eng.to_csv('result_data.csv',header=True,index=False)
