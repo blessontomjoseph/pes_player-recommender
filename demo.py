eee38b55ccb7721cced18d77c33e954d5b5b3d91
blessontomjoseph <blessontomjoseph@gmail.com>
2022-07-08 19:05:26 +0530

    debug up

--- demo.py
+++ demo.py
@@ -2,8 +2,11 @@ import utility
 import streamlit as st
 import pandas as pd
 
-path='E:\kaggle\projects\feature eng PES\pes_player-recommender\result_data.csv'
+path='result_data.csv'
 ml_results=pd.read_csv(path)
+print(ml_results.columns)
+
+
 filters=utility.streamlit(ml_results)
 utility.ex(filters,ml_results)
 
--- utility.py
+++ utility.py
@@ -15,19 +15,19 @@ def similar_players(player_name,data):
 
     our_player=data.loc[data['name'].str.contains(player_name.upper())]
     our_player=our_player.select_dtypes('number')
-    other_players= data.select_dtypes('nuimber')
+    other_players= data.select_dtypes('number')
     distance= our_player - other_players
     distance=np.linalg.norm(distance,axis=1)
     out=data.copy()
     out['p2p_dist']=distance
     players=out.sort_values('p2p_dist',ascending=True)
-    return players.select_dtypes('object').iloc[1:]
+    return players[['name', 'shirt_number', 'team_name', 'league', 'nationality', 'region','foot', 'registered_position', 'ball_color']].iloc[1:]
 
     
 
-def age_below(age,similar):
-    similar=similar[similar['player_age']<age] 
-    return similar
+# def age_below(age,similar):
+#     similar=similar.loc[similar['age']<age] 
+#     return similar
     
 def league(league,similar):
     similar=similar.loc[similar.league==league]
@@ -50,25 +50,25 @@ def ball_color(ball_color,similar):
     return similar
 
 def position (position,similar):
-    similar=similar.loc[similar.position==position]
+    similar=similar.loc[similar.registered_position==position]
     return similar
 
-def ex(filters,data):
-    our_player=data.loc[data.name.str.contains(filters['player_name'].upper())]
-    similar_players=similar_players(our_player,result)
-
-    if filters['age']!='no filter':
-        result=age_below(filters['age'],similar_players)
+def ex(filters,new_data):
+    # our_player=data.loc[data.name.str.contains(filters['player_name'].upper())]
+    similar_player=similar_players(filters['player_name'],new_data)
+    result=similar_player
+    # if filters['age']!='no filter':
+    #     result=age_below(filters['age'],similar_player)
     if filters['league']!='no filter':
-        result=league(filters['league'],similar_players)
+        result=league(filters['league'],similar_player)
     if filters['nationality']!='no filter':   
-        result=nationality(filters['nationality'],similar_players)
+        result=nationality(filters['nationality'],similar_player)
     if filters['foot']!='no filter':
-        result=foot(filters['foot'],similar_players)
+        result=foot(filters['foot'],similar_player)
     if filters['ball_color']!='no filter':
-        result=ball_color(filters['ball_color'],similar_players)
+        result=ball_color(filters['ball_color'],similar_player)
     if filters['position']!='no filter':
-        result=position(filters['position'],similar_players)
+        result=position(filters['position'],similar_player)
     st.write(result)
 
     
@@ -81,13 +81,13 @@ def streamlit(new_data):
     button_clicked = st.button("ok")
     st.sidebar.title('Filter Items')
     
-    age=st.sidebar.slider('Age Below',15,50)
+    # age=st.sidebar.slider('Age Below',15,50)
     league=st.sidebar.selectbox('League',['no filter']+list(new_data.league.unique())) # a filter of a league
     nationality=st.sidebar.selectbox('Nationality',['no filter']+list(new_data.nationality.unique())) #if result=empyu make apro result
     foot=st.sidebar.selectbox('foot',['no filter','Left','Right'])
     ball_color=st.sidebar.selectbox('Ball Color',['no filter']+list(new_data.ball_color.unique()))
     position=st.sidebar.selectbox('Position',['no filter']+list(new_data.registered_position.unique()))
-    fils=['player_name','age','league','nationality','foot','ball_color','position']
-    vals=[player_name,age,league,nationality,foot,ball_color,position]
+    fils=['player_name','league','nationality','foot','ball_color','position']
+    vals=[player_name,league,nationality,foot,ball_color,position]
     filter_pairs={fils[i]:vals[i] for i in range(len(fils)) if vals[i]!='no filter'}
     return filter_pairs
