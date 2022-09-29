2092721ac31948a78364406fdfa3e3061780397e
tom <57592961+blessontomjoseph@users.noreply.github.com>
2022-09-29 09:22:21 +0530

    Update demo_app.py

--- demo_app.py
+++ demo_app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-path= r"result\result.csv"
+path= r"result/result.csv"
 new_features = pd.read_csv(path)
 
 def streamlit(new_features):    
